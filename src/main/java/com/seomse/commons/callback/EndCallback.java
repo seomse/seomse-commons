5d7e3ed3402c2dab0c13c309480dd2254aac8106
macle <ysys86a@gmail.com>
2019-12-05 12:28:00 +0900

    end callback add

---
+++ src/main/java/com/seomse/commons/callback/EndCallback.java
@@ -0,0 +1,18 @@
+package com.seomse.commons.callback;
+
+/**
+ * <pre>
+ *  파 일 명 : EndCallback.java
+ *  설    명 : 종료 콜백
+ *
+ *  작 성 자 : macle
+ *  작 성 일 : 2019.12.05
+ *  버    전 : 1.0
+ *  수정이력 :
+ *  기타사항 :
+ * </pre>
+ * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
+ */
+public interface EndCallback {
+    void callback(Object obj);
+}
