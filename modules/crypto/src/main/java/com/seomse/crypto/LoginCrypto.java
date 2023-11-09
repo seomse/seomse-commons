d6cfdc8692e6c245fb73aa88d73629cb19111ba7
macle <ysys86a@gmail.com>
2023-11-09 23:43:15 +0900

    2023

--- modules/crypto/src/main/java/com/seomse/crypto/LoginCrypto.java
+++ modules/crypto/src/main/java/com/seomse/crypto/LoginCrypto.java
@@ -10,52 +10,68 @@ package com.seomse.crypto;
  */
 @SuppressWarnings("unused")
 public class LoginCrypto {
-	
+
+	public static String [] encryption(String id, String password){
+
+		return encryption(id, password, 16, null);
+	}
+
 	/**
 	 * 로그인 정보 암호화
 	 * @param id string id
 	 * @param password string password
 	 * @return string []  string[0] = id enc , string[1] = password enc
 	 */
-	public static String [] encryption(String id, String password){
+	public static String [] encryption(String id, String password, int keySize ,CharMap charMap){
 		
 		try{
 			//아이디를 이용하여 패스워드 키생성 
-			String encPasswordKey = HashConfusionString.get("MD5",id);
+			String encPasswordKey = HashConfusionString.get("MD5",id, keySize);
+
+			if(charMap != null){
+				encPasswordKey = charMap.change(encPasswordKey);
+			}
 
 			//패스워드암호화
-			String encPassword = StringCrypto.enc(encPasswordKey, password);
+			String encPassword = StringCrypto.enc(encPasswordKey, password, keySize);
 
 			//암호화된 패스워드를 이용하여 아이디 키생성
 			String idKey = HashConfusionString.get("MD5",encPassword);
 			//아이디 암호화
-			String encId = StringCrypto.enc(idKey, id);
+			String encId = StringCrypto.enc(idKey, id, keySize);
 			
 			return new String[] {encId, encPassword};
 		}catch(Exception e){
 			throw new RuntimeException(e);
 		}
 	}
-	
+
+	public static  String [] decryption(String encryptionId, String encryptionPassword){
+		return decryption(encryptionId, encryptionPassword, 16,null);
+	}
+
 	/***
 	 * 로그인 정보 복호화
 	 * @param encryptionId 암호화된 아이디
 	 * @param encryptionPassword 암호화된 패스워드
 	 * @return  string []  string[0] = id  , string[1] = password
 	 */
-	public static  String [] decryption(String encryptionId, String encryptionPassword){
+	public static  String [] decryption(String encryptionId, String encryptionPassword, int keySize, CharMap charMap){
 	
 		try{
 			//암호화된 패스워드를 이용해서 아이디 복호화키생성
-			String decIdKey = HashConfusionString.get("MD5",encryptionPassword);
+			String decIdKey = HashConfusionString.get("MD5",encryptionPassword, keySize);
 
+			if(charMap != null){
+				decIdKey = charMap.change(decIdKey);
+			}
 			//아이디복호화
-			String id = StringCrypto.dec(decIdKey, encryptionId);
+			String id = StringCrypto.dec(decIdKey, encryptionId, keySize);
 
 			//패스워드 복호화 키생성
 			String decPasswordKey = HashConfusionString.get("MD5", id);
 			//패스워드 복호화
-			String password = StringCrypto.dec(decPasswordKey, encryptionPassword);
+			String password = StringCrypto.dec(decPasswordKey, encryptionPassword, keySize);
 
 			return new String[] {id, password};
 		}catch(Exception e){
