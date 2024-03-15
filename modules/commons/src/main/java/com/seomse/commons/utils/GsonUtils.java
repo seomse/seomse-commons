803cd69e37ffd0d80f22bfb065c15a84537e5859
macle <ysys86a@gmail.com>
2024-03-16 04:58:53 +0900

    2024

--- modules/commons/src/main/java/com/seomse/commons/utils/GsonUtils.java
+++ modules/commons/src/main/java/com/seomse/commons/utils/GsonUtils.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * @author macle
  */
@@ -27,4 +30,136 @@ public class GsonUtils {
         return GSON.fromJson(jsonText, JsonArray.class);
     }
 
+    public static JsonArray getArray(String [] array){
+
+        if(array == null){
+            return null;
+        }
+        JsonArray jsonArray = new JsonArray();
+        for(String value : array){
+            jsonArray.add(value);
+        }
+        return jsonArray;
+    }
+
+
+    public static JsonArray getArray(int [] array){
+
+        if(array == null){
+            return null;
+        }
+        JsonArray jsonArray = new JsonArray();
+        for(int value : array){
+            jsonArray.add(value);
+        }
+        return jsonArray;
+    }
+
+
+    public static JsonArray getArray(double [] array){
+
+        if(array == null){
+            return null;
+        }
+        JsonArray jsonArray = new JsonArray();
+        for(double value : array){
+            jsonArray.add(value);
+        }
+        return jsonArray;
+    }
+
+    public static JsonArray getArray(long [] array){
+
+        if(array == null){
+            return null;
+        }
+        JsonArray jsonArray = new JsonArray();
+        for(long value : array){
+            jsonArray.add(value);
+        }
+        return jsonArray;
+    }
+
+
+    public static JsonArray getArray(Number[] array){
+
+        if(array == null){
+            return null;
+        }
+        JsonArray jsonArray = new JsonArray();
+        for(Number value : array){
+            jsonArray.add(value);
+        }
+        return jsonArray;
+    }
+
+    public static String [] getString(JsonArray jsonArray){
+
+        String [] array = new String[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsString();
+        }
+
+        return array;
+    }
+
+    public static int [] getInts(JsonArray jsonArray){
+
+        int [] array = new int[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsInt();
+        }
+
+        return array;
+    }
+
+    public static double [] getDoubles(JsonArray jsonArray){
+
+        double [] array = new double[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsDouble();
+        }
+
+        return array;
+    }
+
+    public static long [] getLongs(JsonArray jsonArray){
+
+        long [] array = new long[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsLong();
+        }
+
+        return array;
+    }
+
+    public static Number [] getNumbers(JsonArray jsonArray){
+
+        Number [] array = new Number[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsNumber();
+        }
+
+        return array;
+    }
+
+    public static BigDecimal [] getBigDecimals(JsonArray jsonArray){
+
+        BigDecimal [] array = new BigDecimal[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsBigDecimal();
+        }
+
+        return array;
+    }
+
+    public static BigInteger[] getBigIntegers(JsonArray jsonArray){
+
+        BigInteger [] array = new BigInteger[jsonArray.size()];
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = jsonArray.get(i).getAsBigInteger();
+        }
+
+        return array;
+    }
 }
